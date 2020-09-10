8706f3da4faebc02fb884c45bf5b450ad45635be
ndgndg91 <ndgndg91@gmail.com>
2020-09-10 20:57:16 +0900

    Create Product class and Product Service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, pipe } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClinet: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClinet.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
