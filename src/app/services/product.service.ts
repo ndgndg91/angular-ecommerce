d5a8f88272484e07666cb266422d520c2696a773
ndgndg91 <ndgndg91@gmail.com>
2020-09-12 22:23:10 +0900

    Call Product Service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, pipe } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products';
 
+  private categoryUrl = 'http://localhost:8080/api/product-category';
   constructor(private httpClinet: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
@@ -19,12 +21,23 @@ export class ProductService {
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClinet.get<GetResponse>(searchUrl).pipe(
+    return this.httpClinet.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response.products)
+    )   
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClinet.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response.productCategoies)
     )   
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   products: Product[];
+}
+
+interface GetResponseProductCategory {
+  productCategoies: ProductCategory[];
 }
