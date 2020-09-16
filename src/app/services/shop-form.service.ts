1c3f82cd0a2f0f695458d959b4c2fc9b3661b5ee
ndgndg91 <ndgndg91@gmail.com>
2020-09-16 21:34:35 +0900

    Create Country, State Classes

--- src/app/services/shop-form.service.ts
+++ src/app/services/shop-form.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopFormService {
 
-  constructor() { }
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
 
