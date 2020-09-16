9ab81d5db1b1b35044dc9d6e16d2e523f5d80990
ndgndg91 <ndgndg91@gmail.com>
2020-09-16 21:02:41 +0900

    Dependent Fields

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ShopFormService } from 'src/app/services/shop-form.service';
 
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -94,4 +95,29 @@ export class CheckoutComponent implements OnInit {
     }
   }
 
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+     else {
+       startMonth = 1;
+     }
+
+     this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+       data => {
+         console.log("Retrieved credit card months:" + JSON.stringify(data));
+         this.creditCardMonths = data;
+       }
+     )
+  }
 }
