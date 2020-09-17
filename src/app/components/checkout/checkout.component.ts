d10c3bb232d9e31b3aa824adc73b8a1735dbab65
donggil91 <donggil@sixshop.com>
2020-09-17 11:37:47 +0900

    Complete Shipping Validation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -48,11 +48,11 @@ export class CheckoutComponent implements OnInit {
       }),
 
       shippingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: ['']
+        street: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
+        city: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
+        state: new FormControl('', [Validators.required]),
+        country: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
       }),
 
       billingAddress: this.formBuilder.group({
@@ -125,6 +125,12 @@ export class CheckoutComponent implements OnInit {
   get firstName() { return this.checkoutFormGroup.get('customer.firstName');  }
   get lastName() { return this.checkoutFormGroup.get('customer.lastName');  }
   get email() { return this.checkoutFormGroup.get('customer.email');  }
+
+  get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street');}
+  get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city');}
+  get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state');}
+  get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode');}
+  get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country');}
 
   copyShippingAddressToBillingAddress(event){
 
